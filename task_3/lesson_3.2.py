acc34dbe6e09743bec16a9b247c3f0e97535da7c
KapaturinD <kapaturin1993d@gmail.com>
2021-12-02 14:00:24 +0600

    Lesson_2.1

---
+++ task_3/lesson_3.2.py
@@ -0,0 +1,6 @@
+def my_func(name, surname, year, city, email, telephone):
+    return ' '.join([name, surname, year, city, email, telephone])
+
+
+print(my_func(surname='Kapaturin', name='Denis', year='1993', city='Nur-Sultan', email='kapaturin1993d@gmail.com',
+              telephone='+77786939607'))
